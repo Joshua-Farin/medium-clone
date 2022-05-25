286b2ede9b35e173cdc62b365bc29c3cdaa81638
Joshua Farin <joshua.k.farin@gmail.com>
2022-05-25 13:38:34 -1000

    helper function for generating urls

--- sanity.js
+++ sanity.js
@@ -10,5 +10,8 @@ export const config = {
     apiVersion: "2021-03-25",
 
     useCdn: process.env.NODE_ENV === "production",
-
 };
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
